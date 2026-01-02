ee717d6cbd900ac71aa6e8d280c8f9b9b5a9377f
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-02 12:24:59 +0530

    Hide/Show Qualifying Header

--- TheoryAdmitCertificate.aspx.cs
+++ TheoryAdmitCertificate.aspx.cs
@@ -175,12 +175,14 @@ public partial class TheoryAdmitCertificate : System.Web.UI.Page
             // Get FacultyName
             DataRowView drv = (DataRowView)e.Item.DataItem;
             string facultyName = drv["FacultyName"].ToString().Trim().ToUpper();
+            string ExamTypeName = drv["ExamTypeName"].ToString().Trim().ToUpper();
             bool hasVocational = drv["HasVocationalSubjects"] != DBNull.Value && Convert.ToBoolean(drv["HasVocationalSubjects"]);
 
 
             HtmlTableCell tdElective1 = e.Item.FindControl("tdElective1") as HtmlTableCell;
             HtmlTableCell tdElective2 = e.Item.FindControl("tdElective2") as HtmlTableCell;
             HtmlTableCell tdElective3 = e.Item.FindControl("tdElective3") as HtmlTableCell;
+            HtmlTableCell tdAdditionalHeader = e.Item.FindControl("tdAdditionalHeader") as HtmlTableCell;
             // Map to Hindi
             string hindiFaculty = string.Empty;
 
@@ -196,7 +198,15 @@ public partial class TheoryAdmitCertificate : System.Web.UI.Page
                     hindiFaculty = "संकाय: <strong>वाणिज्य</strong>";
                     break;
             }
-
+            if (ExamTypeName == "QUALIFYING")
+            {
+                //
+                tdAdditionalHeader.InnerHtml = "Qualifying Subject ";
+            }
+            else
+            {
+                tdAdditionalHeader.InnerHtml = "अतिरिक्त विषय(Additional Subject)";
+            }
             // Show/hide section
             if (facultyName == "VOCATIONAL")
             {
